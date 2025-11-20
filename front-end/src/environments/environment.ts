0a392ecd84a19772eded6a3963e15311e389539b
Fikru Belachew <fikru.belachew01@gmail.com>
2025-11-20 12:25:50 -0500

    Update API endpoints to new server addresses
    Public IP address changed as per current EC2

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  resourceApi: "http://18.117.82.204/resources-api",
-  inventoryApi: "http://18.117.82.204/inventory-api",
-  clientApi: "http://18.117.82.204/clients-api",
-  rentingApi: "http://18.117.82.204/renting-api"
+  resourceApi: "http://3.148.228.203/resources-api",
+  inventoryApi: "http://3.148.228.203/inventory-api",
+  clientApi: "http://3.148.228.203/clients-api",
+  rentingApi: "http://3.148.228.203/renting-api"
 };
